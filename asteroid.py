7ef1f96cfa457b04063c25a2e6729f441215d738
kweysen <kweysen@gmail.com>
2026-03-02 06:13:54 -0800

    New classes and objects added.

---
+++ asteroid.py
@@ -0,0 +1,13 @@
+import circleshape
+import pygame
+import constants
+
+class Asteroid(circleshape.CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius, constants.LINE_WIDTH)
+    
+    def update(self, dt):
+        self.position += self.velocity * dt
--- main.py
+++ main.py
@@ -1,20 +1,64 @@
 import pygame
-import constants
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from logger import log_event
+import sys
 
 def main():
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
     pygame.init()
-    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    # Player is the name of the class, not an instance of it
+    # This must be done before any Player objects are created   
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
+    asteroids = pygame.sprite.Group()
+
+
+    Asteroid.containers = (asteroids,updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    asteroid_field = AsteroidField()
+
     while True:
-        log_state()
+        
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
+        # You may also call the .update() method for every member of a group by calling it on the group itself:
+        updatable.update(dt)
+
+        for obj in asteroids:
+            if player.collides_with(obj):
+                log_event("player_hit")
+                print("Game Over!")
+                sys.exit()
+
+        screen.fill("black")
+        # You can iterate over objects in a group just like any other collection in Python:
+        for obj in drawable:
+            obj.draw(screen)
+
+
+
         pygame.display.flip()
+
+        
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+        log_state()
 
 
 if __name__ == "__main__":
